7d6c05540e9226256a4ba634896b83aa78fc4f7a
orhan gencer <o.gencer@yahoo.com>
2020-04-14 15:33:44 +0300

    arayuz templateler olusturuldu.

--- src/webui/src/app/app.module.ts
+++ src/webui/src/app/app.module.ts
@@ -6,17 +6,44 @@ import {DashboardModule} from "./pages/dashboard/dashboard.module";
 import {ProjectModule} from "./pages/project/project.module";
 import {IssueModule} from "./pages/issue/issue.module";
 
+import {AppLayoutComponent} from "./_layout/app-layout/app-layout.component";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CollapseModule} from "ngx-bootstrap/collapse";
+import {ToastNoAnimation, ToastNoAnimationModule, ToastrModule} from "ngx-toastr";
+import {BsDropdownModule} from "ngx-bootstrap/dropdown";
+import {ModalModule} from "ngx-bootstrap/modal";
+import {BsDatepickerModule} from "ngx-bootstrap/datepicker";
+import {PaginationModule} from "ngx-bootstrap/pagination";
+import {FooterComponent, HeaderComponent, SidebarComponent} from "./_layout";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLayoutComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     DashboardModule,
     ProjectModule,
-    IssueModule
+    IssueModule,
+    CollapseModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    PaginationModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    ToastNoAnimationModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      toastComponent: ToastNoAnimation,
+      maxOpened: 1,
+      autoDismiss: true
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/webui/src/app/app.routing.module.ts
+++ src/webui/src/app/app.routing.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
+import {AppLayoutComponent} from "./_layout";
 
 const routes: Routes = [
   {
-    path: '',
+    path: '', component: AppLayoutComponent,
     children: [
       {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
       {path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardModule'},
@@ -17,5 +18,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
-
+export class AppRoutingModule {
+}
